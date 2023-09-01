6a513e00055d5a9829ffc4be0684d5cf18c8354e
WDEBLUEGRASS <143821751+WDEBLUEGRASS@users.noreply.github.com>
2023-09-01 11:14:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+
+
 #created the main python file
 
 import streamlit
@@ -9,3 +11,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
